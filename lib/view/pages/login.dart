8cfed8c8b61a620af3d640bec2ae68ee847704ef
joaopedroa1 <joaolpq@yahoo.com>
2024-06-10 08:52:47 -0300

    Mudando a tela inicial de login

--- lib/view/pages/login.dart
+++ lib/view/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:corte_barbearia/view/widgets/myAppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -7,15 +8,28 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
+        body: 
+        Stack(
       children: [
         Image(image: AssetImage("assets/TelaFundoCerta.png")),
         Column(
-          children: [Image(image: AssetImage("assets/Group 1.png")),
-           
+          children: [
+            Container(
+              width: double.infinity,
+            child:
+             Column(
+              children: [
+                Image(image: AssetImage("assets/Group 1.png")),
+                Text('Funcionários', style: GoogleFonts.roboto(color: Colors.white, fontSize: 30)),
+            ])),
+            Column(children: [
+              TextField(
+                
+              )
+            ],)
+          ]),
           ],
         )
-      ],
-    ));
+    );
   }
 }
